8fd0c96fad402352a09f2f16fa055b55885b3356
최원식 <74888408+king258436@users.noreply.github.com>
2023-04-01 19:30:11 +0900

    Update 3-2.py

--- 3-2.py
+++ 3-2.py
@@ -6,7 +6,5 @@ def seq_search(a: Sequence, key: Any) -> int:
             return i
     return -1
 
-#생략인 부분 추가로 만들어야함 
-
-arr = [1,2,3,4,5,6] 
-print("3의 위치 : ", seq_search(arr,3))
+arr = [1,2,3,4,5,6,7,8] 
+print("3의 위치 : ", seq_search(arr,5))
